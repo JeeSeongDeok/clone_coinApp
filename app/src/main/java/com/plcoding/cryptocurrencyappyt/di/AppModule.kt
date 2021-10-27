2f6a976727b696d52876490d6ed083a59886587e
HeyDosomething <bh939@naver.com>
2021-10-27 11:49:42 +0900

    Use Case

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.plcoding.cryptocurrencyappyt.di
+
+object AppModule {
+}
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,33 @@
+package com.plcoding.cryptocurrencyappyt.domain.use_case.get_coin
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoin
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+    /*
+     *  operator invoke: 이 함수를 불러낼 때 객체.invoke()를 하지 않아도 객체()로 호출 가능하다
+     */
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow{
+        try{
+            // 로딩 중 작업표시줄을 띄우기 위해서
+            emit(Resource.Loading<CoinDetail>())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "예상하지 못한 에러"))
+        } catch (e: IOException) {
+            emit(Resource.Error<CoinDetail>("인터넷 연결 실패"))
+        }
+    }
+}
